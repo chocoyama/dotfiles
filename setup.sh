ff9a599470ff7f45d49cca0303c27a5b4f6d1546
Takuya Yokoyama <takyokoy@yahoo-corp.jp>
2018-04-12 16:32:39 +0900

    setupスクリプトを追加

---
+++ setup.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# brew
+if type brew > /dev/null 2>&1; then
+	echo "[INFO] brew found."
+else
+	echo "[INFO] install brew."
+	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+# nvm
+if type nvm > /dev/null 2>&1; then
+	echo "[INFO] nvm found."
+else
+	echo "[INFO] install nvm."
+	brew install nvm
+fi
+
+# compsoer
+if type composer > /dev/null 2>&1; then
+	echo "[INFO] composer found."
+else
+	echo "[INFO] install composer."
+	curl -sS https://getcomposer.org/installer | php
+	mv composer.phar /usr/local/bin/composer
+fi
